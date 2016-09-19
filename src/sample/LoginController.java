c2e1ddef744af9c06f4a67a615ea7e18323cff5d
jianhua222 <1993222king@gmail.com>
2016-09-19 18:52:33 -0400

    Login pressed works

---
+++ src/sample/LoginController.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+/**
+ * Controller for login screen
+ */
+public class LoginController {
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,74 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+public class Main extends Application {
+
+    /** the main container for the application window */
+    private Stage mainScreen;
+
+    /** the main layout for the main window */
+    private Pane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        mainScreen = primaryStage;
+        initRootLayout(mainScreen);
+    }
+
+    /**
+     * Initialize the main screen for the application.  Most other views will be shown in this screen.
+     *
+     * @param mainScreen  the main Stage window of the application
+     */
+    private void initRootLayout(Stage mainScreen) {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("../sample/welcome.fxml"));
+            rootLayout = loader.load();
+
+            // Give the controller access to the main app.
+            WelcomeController controller = loader.getController();
+            controller.setMainApp(this);
+
+            // Set the Main App title
+            mainScreen.setTitle("Clean Water Crowdsourcing");
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            mainScreen.setScene(scene);
+            mainScreen.show();
+
+
+        } catch (IOException e) {
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void showLoginScreen() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle(" LOGIN");
+            primaryStage.setScene(new Scene(root, 600, 400));
+            primaryStage.show();
+
+        } catch (IOException e) {
+
+        }
+    }
+}
