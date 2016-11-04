aab478ec257a2459ca9d20a6cacd6d0353d50b4e
Jai Chauhan <checkmate180@gmail.com>
2016-11-04 13:12:29 -0400

    Manager.java created

---
+++ src/sample/model/Manager.java
@@ -0,0 +1,10 @@
+package sample.model;
+
+/**
+ * Created by Jai Chauhan on 11/4/2016.
+ */
+public class Manager extends Worker {
+    public Manager(String username, String password, String type) {
+        super(username, password, type);
+    }
+}
