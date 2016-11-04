5aa5dab5f4a988542914f80ef1f574f0404537eb
Jai Chauhan <checkmate180@gmail.com>
2016-11-04 11:58:18 -0400

    Woops forgot Worker.java

---
+++ src/sample/model/Worker.java
@@ -0,0 +1,15 @@
+package sample.model;
+
+/**
+ * Created by Jai Chauhan on 10/31/2016.
+ */
+
+/**
+ * Constructor for woker
+ */
+public class Worker extends User {
+    public Worker(String username, String password, String type) {
+        super(username, password, type);
+    }
+
+}
